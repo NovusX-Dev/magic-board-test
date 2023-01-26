0ee2ca006d2c348576b00b7b344f21556c2556fd
NovusX-Dev <novusxdev@outlook.com>
2023-01-26 04:45:52 -0300

    Nodes
    - node script created
    - route script created
    - basic instantiation of nodes

---
+++ Assets/Script/Node.cs
@@ -0,0 +1,45 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace MagicBoard
+{
+    public class Node : MonoBehaviour
+    {
+        #region Exposed_Variables
+
+        [SerializeField] private TextMeshProUGUI numberText = null;
+
+        #endregion
+
+        #region Private_Variables
+
+        private int _nodeId;
+        private Node _connectedNode;
+
+        #endregion
+
+        #region Public_Variables
+
+        #endregion
+
+        #region Unity_Calls
+
+        #endregion
+
+        #region Private_Methods
+
+        #endregion
+
+        #region Public_Methods
+
+        public void SetNodeId(int id)
+        {
+            _nodeId = id;
+            numberText.text = id.ToString();
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ Assets/Script/RouteGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MagicBoard
+{
+    public class RouteGenerator : MonoBehaviour
+    {
+        #region Exposed_Variables
+
+        [Header("Nodes")]
+        [SerializeField] private int nodesAmount = 100;
+        [SerializeField] private Transform nodesContainer = null;
+        [SerializeField] private Node nodePrefab = null;
+
+        #endregion
+
+        #region Private_Variables
+
+        private Transform[] _nodes;
+        private List<Node> _nodesList = new List<Node>();
+
+        #endregion
+
+        #region Public_Variables
+
+        #endregion
+
+        #region Unity_Calls
+
+        private void Start()
+        {
+            FillNodes();
+        }
+
+        #endregion
+
+        #region Private_Methods
+
+        private void FillNodes()
+        {
+            _nodesList.Clear();
+
+            for (int i = 0; i < nodesAmount; i++)
+            {
+                var node = Instantiate(nodePrefab, nodesContainer);
+                node.gameObject.name = $"Node {i + 1}";
+                node.SetNodeId(i + 1);
+            }
+        }
+
+        #endregion
+
+        #region Public_Methods
+
+        #endregion
+
+
+    }
+}
